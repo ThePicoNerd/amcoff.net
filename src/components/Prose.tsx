3b96745e7afcc3fe32a668413f6ffe462e2a3323
ThePicoNerd <ake@amcoff.net>
2022-03-02 20:21:24 +0100

    syntax highlighting

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -1,6 +1,7 @@
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import React, { FunctionComponent, ReactNode } from "react";
 import Image from "next/image";
+import SyntaxHighlighter from "react-syntax-highlighter";
 import Warning from "./Warning";
 import styles from "./Prose.module.scss";
 
@@ -8,9 +9,29 @@ export interface Props {
   source: MDXRemoteSerializeResult;
 }
 
+const Code: FunctionComponent<{ className: string }> = ({
+  className,
+  ...props
+}) => {
+  const language = /language-(\w+)/.exec(className ?? "")?.[1];
+
+  return language ? (
+    <SyntaxHighlighter
+      language={language}
+      showLineNumbers
+      wrapLongLines
+      useInlineStyles={false}
+      {...props}
+    />
+  ) : (
+    <code className={className} {...props} />
+  );
+};
+
 const components: Record<string, ReactNode> = {
   Warning,
   Image,
+  code: Code,
 };
 
 const Prose: FunctionComponent<Props> = ({ source }) => (
