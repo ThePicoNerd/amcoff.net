022ddc34c3ec284865707f20cc97366820f55500
akeamc <ake@amcoff.net>
2024-03-26 21:11:10 +0100

    Image blur placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
+import profile from "../images/profile.jpg";
 
 export default function Page() {
   return (
     <main className="max-w-screen-xl mx-auto flex items-center justify-center min-h-screen p-4">
       <div className="text-center">
         <Image
-          src="/profile.jpg"
+          src={profile}
           width={256}
           height={256}
           alt="Åke"
+          placeholder="blur"
           className="rounded-full mx-auto"
         />
         <h1 className="text-4xl font-bold tracking-tighter my-4">Åke Amcoff</h1>
