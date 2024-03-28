ef1dee6f11913d60ccba0446722d27844f54c0ab
akeamc <ake@amcoff.net>
2024-03-28 14:48:44 +0100

    update background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { martianMono } from "@/app/fonts";
-import { useCallback, useEffect, useRef } from "react";
+import classNames from "classnames";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function randomHexByte() {
   return Math.floor(Math.random() * 256)
@@ -15,6 +16,7 @@ function Row(props: { x: number; y: number }) {
   const textRef = useRef<SVGTextElement>(null);
   const frameRef = useRef<number | null>(null);
   const updateCountRef = useRef(0);
+  const t0Ref = useRef(0);
 
   const draw = useCallback((t: DOMHighResTimeStamp) => {
     const element = textRef.current;
@@ -23,18 +25,21 @@ function Row(props: { x: number; y: number }) {
     const bytes = element.textContent!.split(" ");
 
     let updates = 0;
-    while (updateCountRef.current < t * UPDATE_FREQ) {
+    while (updateCountRef.current < (t - t0Ref.current) * UPDATE_FREQ) {
       bytes[Math.floor(Math.random() * bytes.length)] = randomHexByte();
       updateCountRef.current++;
       updates++;
 
       if (updates > 10) {
+        t0Ref.current = t;
+        updateCountRef.current = 0;
         break;
       }
     }
-    bytes[Math.floor(Math.random() * bytes.length)] = randomHexByte();
 
-    element.textContent = bytes.join(" ");
+    if (updates > 0) {
+      element.textContent = bytes.join(" ");
+    }
 
     frameRef.current = requestAnimationFrame(draw);
   }, []);
@@ -56,12 +61,18 @@ function Row(props: { x: number; y: number }) {
       className={martianMono.className + " fill-neutral-700"}
       style={{ fontSize: 8 }}
     >
-      {Array.from({ length: 8 }, () => "00").join(" ")}
+      {Array.from({ length: 16 }, () => "00").join(" ")}
     </text>
   );
 }
 
 export default function Background() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+  }, []);
+
   return (
     <svg className="fixed inset-0 w-full h-full -z-10">
       <defs>
@@ -69,24 +80,32 @@ export default function Background() {
           id="text"
           x="0"
           y="0"
-          width="134.41"
-          height="120"
+          width="268.81"
+          height="240"
           patternUnits="userSpaceOnUse"
         >
-          <rect
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            className="fill-neutral-900"
-          />
-          {Array.from({ length: 8 }, (_, i) => (
+          {Array.from({ length: 16 }, (_, i) => (
             <Row key={i} x={0} y={(i + 1) * 15} />
           ))}
         </pattern>
       </defs>
-
-      <rect fill="url(#text)" stroke="black" width="100%" height="100%" />
+      <rect
+        x="0"
+        y="0"
+        width="100%"
+        height="100%"
+        className="fill-neutral-900"
+      />
+      <rect
+        fill="url(#text)"
+        stroke="black"
+        width="100%"
+        height="100%"
+        className={classNames(
+          "transition-all duration-500 origin-center ease-in-out",
+          show ? "scale-100 opacity-100" : "scale-105 opacity-0",
+        )}
+      />
     </svg>
   );
 }
