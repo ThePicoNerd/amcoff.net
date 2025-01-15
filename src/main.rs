aaf57c0e9be01f82a01b9efdb92c5c26a7b3effd
akeamc <ake@amcoff.net>
2025-01-15 13:13:00 +0100

    add ipa

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 use std::sync::LazyLock;
 
 use axum::{
-    http::header::{CACHE_CONTROL, CONTENT_TYPE},
+    http::{
+        header::{CACHE_CONTROL, CONTENT_TYPE},
+        HeaderValue, StatusCode,
+    },
     response::IntoResponse,
     routing::get,
     Router,
 };
 use prometheus::{register_int_counter, IntCounter};
-use tower_http::compression::CompressionLayer;
+use tower_http::{compression::CompressionLayer, set_header::SetResponseHeaderLayer};
 
 static ROBOT_COUNTER: LazyLock<IntCounter> =
     LazyLock::new(|| register_int_counter!("robots", "/robots.txt counter").unwrap());
 
 async fn robots() -> impl IntoResponse {
     ROBOT_COUNTER.inc();
-    ([(CACHE_CONTROL, "no-cache")], "lol")
+    "lol"
 }
 
 async fn index() -> impl IntoResponse {
@@ -29,10 +32,17 @@ async fn index() -> impl IntoResponse {
 
 async fn metrics() -> impl IntoResponse {
     let metric_families = prometheus::gather();
-    let text = prometheus::TextEncoder::new()
+    prometheus::TextEncoder::new()
         .encode_to_string(&metric_families)
-        .unwrap();
-    ([(CACHE_CONTROL, "no-cache")], text)
+        .unwrap()
+}
+
+async fn method_not_allowed() -> impl IntoResponse {
+    (StatusCode::METHOD_NOT_ALLOWED, "Method not allowed")
+}
+
+async fn not_found() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "Not found")
 }
 
 #[tokio::main]
@@ -43,7 +53,13 @@ async fn main() {
         .route("/", get(index))
         .route("/robots.txt", get(robots))
         .route("/metrics", get(metrics))
-        .layer(CompressionLayer::new());
+        .method_not_allowed_fallback(method_not_allowed)
+        .fallback(not_found)
+        .layer(CompressionLayer::new())
+        .layer(SetResponseHeaderLayer::if_not_present(
+            CACHE_CONTROL,
+            HeaderValue::from_static("no-cache"),
+        ));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
